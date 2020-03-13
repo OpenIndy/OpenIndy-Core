833dbfb1080d06c9238fc6a77304ff1156db7696
Schulte, Eberhard <schulte@sigma3d.de>
2020-03-13 14:16:02 +0100

    OI-487: add vi, vj, vk to map

--- src/util/util.cpp
+++ src/util/util.cpp
@@ -320,9 +320,9 @@ void init(){
     observationDisplayAttributesMap.insert(eObservationDisplayVZ, "vz");
     observationDisplayAttributesMap.insert(eObservationDisplayV, "v");
     observationDisplayAttributesMap.insert(eObservationDisplayVR, "vr");
-//    observationDisplayAttributesMap.insert(eObservationDisplayVI, "vi");
-//    observationDisplayAttributesMap.insert(eObservationDisplayVJ, "vj");
-//    observationDisplayAttributesMap.insert(eObservationDisplayVK, "vk");
+    observationDisplayAttributesMap.insert(eObservationDisplayVI, "vi");
+    observationDisplayAttributesMap.insert(eObservationDisplayVJ, "vj");
+    observationDisplayAttributesMap.insert(eObservationDisplayVK, "vk");
     observationDisplayAttributesMap.insert(eObservationDisplayIsUsed, "used");
     observationDisplayAttributesMap.insert(eObservationDisplayIsDummyPoint, "dummy point");
 
