3f2069113d3b85ef91cbfa8303c885defe014e96
Schulte, Eberhard <schulte@sigma3d.de>
2023-01-11 10:54:03 +0100

    OI-905: debug

--- src/sensorworker.cpp
+++ src/sensorworker.cpp
@@ -416,6 +416,13 @@ void SensorWorker::measure(int geomId, MeasurementConfig mConfig){
         QJsonObject request;
         request.insert("method", "measure");
         request.insert("geomId", geomId);
+
+        QDomDocument mConfigXml("measurementConfig");
+        //add mConfig to document as xml
+        QDomElement root = mConfig.toOpenIndyXML(mConfigXml);
+        mConfigXml.appendChild(root);
+        emit this->sensorMessage(mConfigXml.toString(),MessageTypes::eInformationMessage);
+
         this->sensor->setMeasurementConfig(mConfig);
         QJsonObject status = this->sensor->performAsyncSensorCommand(request);
         if(status.value("status").toString().compare("blocked") == 0) {
